e2d619cb077dbbda5f953b6dfb7954011ed99030
Kerr Hamilton <kerr9928@gmail.com>
2024-08-21 21:26:31 +0000

    Add multiple functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,45 @@
  */
 
 // Vanilla JavaScript Function
+//function addTwoNumbers(a, b) {
+   // code block
+
+   //return a + b
+//}
+
+//let sum = addTwoNumbers(5,5)
+//console.log(sum)
 
 // Arrow Function With Parameters
+//const addTwoNumbers = (a, b) => {
+    // code block
+ 
+    //return a + b
+// /}
+ 
+ //let sum = addTwoNumbers(7,5)
+ //console.log(sum)
 
 // Single Line Arrow Function With Parameters
 
+//const addTwoNumbers = (a,b) => a + b;
+//let sum = addTwoNumbers(6,5)
+//console.log(sum)
+
 // Implicit Returns
 
+//const saySomthing = message => console.log(message)
+//saySomthing("Hello!!");
+
+const sayHello =  () => console.log("Hello")
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+
+   `<p>
+        Hello
+    </p>`
+)
+console.log(returnMultipleLines());
